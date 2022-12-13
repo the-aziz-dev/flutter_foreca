12a0d71f1e61d1019cbd28a494ebe9fcb393fe27
the-aziz <aziz.sheikhehpour.id@gmail.com>
2022-12-13 11:25:43 +0330

    Update weather.dart to get data base on city name

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -5,6 +5,13 @@ const apiKey = '119c85091039a4cdfea5fb1abae83b52';
 const openWeatherMapUrl = 'https://api.openweathermap.org/data/2.5/forecast';
 
 class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapUrl?q=$cityName&appid=$apiKey&units=metric');
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   Future<dynamic> getLocationWeather() async {
     Location location = Location();
     await location.getCurrentLocation();
