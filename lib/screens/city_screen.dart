d75825bd6fcb9cd2c400c00d8253a1bc70c05d5f
the-aziz <aziz.sheikhehpour.id@gmail.com>
2022-12-13 09:14:15 +0330

    Update city_screen.dart to add city name TextField

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -35,7 +35,29 @@ class _CityScreenState extends State<CityScreen> {
               ),
               Container(
                 padding: const EdgeInsets.all(20.0),
-                child: null,
+                child: const TextField(
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    icon: Icon(
+                      Icons.location_city,
+                      color: Colors.white,
+                    ),
+                    hintText: 'Enter City Name',
+                    hintStyle: TextStyle(
+                      color: Colors.grey,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10.0),
+                      ),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
               ),
               TextButton(
                 onPressed: () {},
